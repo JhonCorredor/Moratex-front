a12df87f28a3c0165aaa1c41cb242b8e8bceff84
Jhon <jwca19@gmail.com>
2023-10-10 19:19:37 -0500

    Ajuste de frm base y validacion rolFrm

--- src/app/parameters/parameters-routing.module.ts
+++ src/app/parameters/parameters-routing.module.ts
@@ -2,35 +2,222 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'bancos', loadChildren: () => import('./general-parameter/general-parameter.module').then(m => m.GeneralParameterModule), 
-  data: { ruta: 'Bancos', titulo: " Banco" , modulo :  "Parametros" , iconModule: "fas fa-cogs" } },
-  { path: 'paises', loadChildren: () => import('./general-parameter/general-parameter.module').then(m => m.GeneralParameterModule), 
-  data: { ruta: 'Paises', titulo: " Paises" , modulo :  "Parametros" , iconModule: "fas fa-cogs"} },
-  { path: 'medidas', loadChildren: () => import('./general-parameter/general-parameter.module').then(m => m.GeneralParameterModule), 
-  data: { ruta: 'Medidas', titulo: " Medidas" , modulo :  "Parametros" , iconModule: "fas fa-cogs"} },
-  { path: 'cargos', loadChildren: () => import('./general-parameter/general-parameter.module').then(m => m.GeneralParameterModule),
-   data: { ruta: 'Cargos', titulo: "Cargos"  , modulo :  "Parametros"} },
+  {
+    path: 'bancos',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'Bancos',
+      titulo: ' Banco',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'paises',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'Paises',
+      titulo: ' Paises',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'medidas',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'Medidas',
+      titulo: ' Medidas',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'cargos',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: { ruta: 'Cargos', titulo: 'Cargos', modulo: 'Parametros' },
+  },
+  {
+    path: 'bodegas',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: { ruta: 'Bodegas', titulo: 'Bodegas', modulo: 'Parametros' },
+  },
+  {
+    path: 'unidadesMedidas',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'UnidadesMedidas',
+      titulo: 'Unidades de Medidas',
+      modulo: 'Parametros',
+    },
+  },
+  {
+    path: 'categorias',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'Categorias',
+      titulo: 'Categorias',
+      modulo: 'Parametros',
+    },
+  },
+  {
+    path: 'tiposEstados',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'TiposEstados',
+      titulo: 'TiposEstados',
+      modulo: 'Parametros',
+    },
+  },
+  {
+    path: 'mediosPagos',
+    loadChildren: () =>
+      import('./general-parameter/general-parameter.module').then(
+        (m) => m.GeneralParameterModule
+      ),
+    data: {
+      ruta: 'MediosPagos',
+      titulo: 'MediosPagos',
+      modulo: 'Parametros',
+    },
+  },
 
-  { path: 'departamentos', loadChildren: () => import('./general-key-parameter/general-key-parameter.module').then(m => m.GeneralKeyParameterModule), 
-  data: { ruta: 'Departamentos', titulo: "Departamentos"   , foreignKeyRuta:"pais"  , foreignKeyTitle:'Pais'  , serviceKey: "Paises" , modulo :  "Parametros" , iconModule: "fas fa-cogs"}  },
-  { path: 'sedes', loadChildren: () => import('./general-key-parameter/general-key-parameter.module').then(m => m.GeneralKeyParameterModule), 
-  data: { ruta: 'Sedes', titulo: "Sedes"   , foreignKeyRuta:"empresa"  , foreignKeyTitle:'Empresa'  , serviceKey: "Empresas" , modulo :  "Parametros" , iconModule: "fas fa-cogs"}  },
-  { path: 'areas', loadChildren: () => import('./general-key-parameter/general-key-parameter.module').then(m => m.GeneralKeyParameterModule), 
-  data: { ruta: 'Areas', titulo: "Areas"   , foreignKeyRuta:"sede"  , foreignKeyTitle:'Sede'  , serviceKey: "Sedes" , modulo :  "Parametros" , iconModule: "fas fa-cogs"}  },
-  { path: 'ciudades', loadChildren: () => import('./general-key-parameter/general-key-parameter.module').then(m => m.GeneralKeyParameterModule), 
-  data: { ruta: 'Ciudades', titulo: "Ciudades"  , foreignKeyRuta:"departamento"  , foreignKeyTitle:'Departamento'   , serviceKey: "Departamentos" , modulo :  "Parametros" , iconModule: "fas fa-cogs"}  },
+  {
+    path: 'estados',
+    loadChildren: () =>
+      import('./general-key-parameter/general-key-parameter.module').then(
+        (m) => m.GeneralKeyParameterModule
+      ),
+    data: {
+      ruta: 'Estados',
+      titulo: 'Estados',
+      foreignKeyRuta: 'tipoEstado',
+      foreignKeyTitle: 'Tipo Estado',
+      serviceKey: 'TiposEstados',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'departamentos',
+    loadChildren: () =>
+      import('./general-key-parameter/general-key-parameter.module').then(
+        (m) => m.GeneralKeyParameterModule
+      ),
+    data: {
+      ruta: 'Departamentos',
+      titulo: 'Departamentos',
+      foreignKeyRuta: 'pais',
+      foreignKeyTitle: 'Pais',
+      serviceKey: 'Paises',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'sedes',
+    loadChildren: () =>
+      import('./general-key-parameter/general-key-parameter.module').then(
+        (m) => m.GeneralKeyParameterModule
+      ),
+    data: {
+      ruta: 'Sedes',
+      titulo: 'Sedes',
+      foreignKeyRuta: 'empresa',
+      foreignKeyTitle: 'Empresa',
+      serviceKey: 'Empresas',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'areas',
+    loadChildren: () =>
+      import('./general-key-parameter/general-key-parameter.module').then(
+        (m) => m.GeneralKeyParameterModule
+      ),
+    data: {
+      ruta: 'Areas',
+      titulo: 'Areas',
+      foreignKeyRuta: 'sede',
+      foreignKeyTitle: 'Sede',
+      serviceKey: 'Sedes',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
+  {
+    path: 'ciudades',
+    loadChildren: () =>
+      import('./general-key-parameter/general-key-parameter.module').then(
+        (m) => m.GeneralKeyParameterModule
+      ),
+    data: {
+      ruta: 'Ciudades',
+      titulo: 'Ciudades',
+      foreignKeyRuta: 'departamento',
+      foreignKeyTitle: 'Departamento',
+      serviceKey: 'Departamentos',
+      modulo: 'Parametros',
+      iconModule: 'fas fa-cogs',
+    },
+  },
 
-
-  
-  { path: 'archivo', loadChildren: () => import('./archivo/archivo.module').then(m => m.ArchivoModule) },
-  { path: "proveedores" , loadChildren : () => import('./proveedor/proveedores.module').then(m => m.ProveedoresModule) },
-  { path: "empresas" , loadChildren : () => import('./empresas/empresas.module').then(m => m.EmpresasModule) },
-  { path: "empleados" , loadChildren : () => import('./empleados/empleados.module').then(m => m.EmpleadosModule) },
-  { path: 'factura-compra' , loadChildren : () => import('./factura-compra/factura-compra.module').then(m => m.FacturaCompraModule) }
+  {
+    path: 'archivo',
+    loadChildren: () =>
+      import('./archivo/archivo.module').then((m) => m.ArchivoModule),
+  },
+  {
+    path: 'proveedores',
+    loadChildren: () =>
+      import('./proveedor/proveedores.module').then((m) => m.ProveedoresModule),
+  },
+  {
+    path: 'empresas',
+    loadChildren: () =>
+      import('./empresas/empresas.module').then((m) => m.EmpresasModule),
+  },
+  {
+    path: 'empleados',
+    loadChildren: () =>
+      import('./empleados/empleados.module').then((m) => m.EmpleadosModule),
+  },
+  {
+    path: 'factura-compra',
+    loadChildren: () =>
+      import('./factura-compra/factura-compra.module').then(
+        (m) => m.FacturaCompraModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ParametersRoutingModule { }
+export class ParametersRoutingModule {}
--- src/app/security/roles/roles-formulario-form/roles-formulario-formcomponent.ts
+++ src/app/security/roles/roles-formulario-form/roles-formulario-formcomponent.ts
@@ -5,18 +5,20 @@ import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 import { LANGUAGE_DATATABLE } from 'src/app/admin/datatable.language';
 import { DatatableParameter } from 'src/app/admin/datatable.parameters';
-import { HelperService, Messages, MessageType } from 'src/app/admin/helper.service';
+import {
+  HelperService,
+  Messages,
+  MessageType,
+} from 'src/app/admin/helper.service';
 import { BotonesComponent } from 'src/app/general/botones/botones.component';
 import { RolesFormularioService } from '../rolesFormularios.service';
 
 @Component({
   selector: 'app-roles-formulario-form',
   templateUrl: './roles-formulario-form.component.html',
-  styleUrls: ['./roles-formulario-form.component.css']
+  styleUrls: ['./roles-formulario-form.component.css'],
 })
-
 export class RolesFormularioFormComponent implements OnInit {
-
   @ViewChild('botonesDatatable') botonesDatatable!: BotonesComponent;
   @ViewChild(DataTableDirective) dtElement!: DataTableDirective;
   public dtTrigger: Subject<any> = new Subject();
@@ -27,26 +29,28 @@ export class RolesFormularioFormComponent implements OnInit {
 
   @Input() Rol_Id: any = null;
 
-  public frmRolesFormulario! : FormGroup;
-  public statusForm : boolean = true
-  public listFormularios : any[] = [];
+  public frmRolesFormulario!: FormGroup;
+  public statusForm: boolean = true;
+  public listFormularios: any[] = [];
 
-  constructor(public routerActive: ActivatedRoute, 
-    private service : RolesFormularioService , 
-    private helperService: HelperService,  
-    private fb: FormBuilder) { }
+  constructor(
+    public routerActive: ActivatedRoute,
+    private service: RolesFormularioService,
+    private helperService: HelperService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.buildForm()
+    this.buildForm();
     this.cargarDatatable();
   }
 
   buildForm(): void {
     this.frmRolesFormulario = this.fb.group({
       Formulario_Id: [null, [Validators.required]],
-      Estado : [true, [Validators.required]]
+      Estado: [true, [Validators.required]],
     });
-    this.buildSelect()
+    this.buildSelect();
   }
 
   ngAfterViewInit() {
@@ -57,41 +61,49 @@ export class RolesFormularioFormComponent implements OnInit {
     this.dtTrigger.unsubscribe();
   }
 
-  buildSelect(){
-    this.service.getAll("Formularios").subscribe(({data}) => {
+  buildSelect() {
+    this.service.getAll('Formularios').subscribe(({ data }) => {
       this.listFormularios = data;
     });
   }
 
-
   save() {
     if (this.frmRolesFormulario.invalid) {
-      this.statusForm  = false
+      this.statusForm = false;
       this.helperService.showMessage(MessageType.WARNING, Messages.EMPTYFIELD);
       return;
     }
-    let data  = { 
+    let data = {
       id: this.Id ?? 0,
       ...this.frmRolesFormulario.value,
-     Rol_Id : this.Rol_Id,
+      Rol_Id: this.Rol_Id,
     };
-    this.service.save(this.Id, data).subscribe(l => {
-      if (!l.status) {
-        this.helperService.showMessage(MessageType.ERROR, Messages.SAVEERROR)
-      } else {
-        this.refrescarTabla();
-        this.frmRolesFormulario.reset();
-        this.Id = null;
-        this.frmRolesFormulario.controls.Estado.setValue(true)
-        this.helperService.showMessage(MessageType.SUCCESS, Messages.SAVESUCCESS)
+    this.service.save(this.Id, data).subscribe(
+      (response) => {
+        if (response.status) {
+          this.refrescarTabla();
+          this.frmRolesFormulario.reset();
+          this.Id = null;
+          this.frmRolesFormulario.controls.Estado.setValue(true);
+          this.helperService.showMessage(
+            MessageType.SUCCESS,
+            Messages.SAVESUCCESS
+          );
+        }
+      },
+      (error) => {
+        this.helperService.showMessage(
+          MessageType.WARNING,
+          error.error.message
+        );
       }
-    })
+    );
   }
 
   refrescarTabla() {
-    if(typeof this.dtElement.dtInstance != 'undefined'){
+    if (typeof this.dtElement.dtInstance != 'undefined') {
       this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        dtInstance.ajax.reload()
+        dtInstance.ajax.reload();
       });
     }
   }
@@ -99,84 +111,103 @@ export class RolesFormularioFormComponent implements OnInit {
   cargarDatatable() {
     const that = this;
     that.opcionesDataTable = {
-        serverSide: true,
-        processing: true,
-        ordering: true,
-        responsive: true,
-        paging: true,
-        order: [0, 'desc'],
-        ajax: (dataTablesParameters: any, callback : any) => {
-          let pageNumber = (dataTablesParameters.start / dataTablesParameters.length) + 1
-          var data = new DatatableParameter();
-          data.pageNumber = pageNumber.toString();
-          data.pageSize = dataTablesParameters.length.toString();
-          data.filter = dataTablesParameters.search.value;
-          data.columnOrder = that.helperService.capitalizeFirstLetter(dataTablesParameters.columns[dataTablesParameters.order[0].column].data.toString());;
-          data.directionOrder = dataTablesParameters.order[0].dir;
-          data.foreignKey = this.Rol_Id;
-          this.service.getAllRolesFormulario(data).subscribe((res: any) => {
-            callback({
-              recordsTotal: res.meta.totalCount,
-              recordsFiltered: res.meta.totalCount,
-              draw: dataTablesParameters.draw,
-              data: res.data
-            });
+      serverSide: true,
+      processing: true,
+      ordering: true,
+      responsive: true,
+      paging: true,
+      order: [0, 'desc'],
+      ajax: (dataTablesParameters: any, callback: any) => {
+        let pageNumber =
+          dataTablesParameters.start / dataTablesParameters.length + 1;
+        var data = new DatatableParameter();
+        data.pageNumber = pageNumber.toString();
+        data.pageSize = dataTablesParameters.length.toString();
+        data.filter = dataTablesParameters.search.value;
+        data.columnOrder = that.helperService.capitalizeFirstLetter(
+          dataTablesParameters.columns[
+            dataTablesParameters.order[0].column
+          ].data.toString()
+        );
+        data.directionOrder = dataTablesParameters.order[0].dir;
+        data.foreignKey = this.Rol_Id;
+        this.service.getAllRolesFormulario(data).subscribe((res: any) => {
+          callback({
+            recordsTotal: res.meta.totalCount,
+            recordsFiltered: res.meta.totalCount,
+            draw: dataTablesParameters.draw,
+            data: res.data,
           });
+        });
+      },
+      language: LANGUAGE_DATATABLE,
+      columns: [
+        {
+          title: 'Formulario',
+          data: 'formulario',
         },
-        language: LANGUAGE_DATATABLE,
-        columns: [
-            {
-              title: "Formulario",
-              data: 'formulario',
-            },
 
-            {
-              title: "Estado",
-              data: 'estado',
-              render: function(item: any) {
-                if (item) {
-                  return "<label class='text-center badge badge-success'>Activo</label>";
-                } else {
-                  return "<label class='text-center badge badge-danger'>Inactivo</label>";
-                }
-              }
-            },
-            {
-              title: "Acciones",
-              orderable: false,
-              width: '300px',
-              data: "id",
-              render: function(id : any, type : any, row : any) {
-                const boton = that.botonesDatatable;
-                return boton.botonesDropdown.nativeElement.outerHTML.split('$id').join(id);
-              },
-              className: "pl-1 pr-0 text-center",
-              responsivePriority: 7
+        {
+          title: 'Estado',
+          data: 'estado',
+          render: function (item: any) {
+            if (item) {
+              return "<label class='text-center badge badge-success'>Activo</label>";
+            } else {
+              return "<label class='text-center badge badge-danger'>Inactivo</label>";
             }
-        ],
-        drawCallback: (settings : any) => {
-          $('.btn-dropdown-modificar').off().on('click', (event : any) => {
-            this.service.getRolesFormularioById(event.target.dataset.id).subscribe(({data}) => {
-              this.Id = data.id;
-              this.frmRolesFormulario.controls.ProductoId.setValue(data.productoId);
-              this.frmRolesFormulario.controls.Estado.setValue(data.estado);
-            })
+          },
+        },
+        {
+          title: 'Acciones',
+          orderable: false,
+          width: '300px',
+          data: 'id',
+          render: function (id: any, type: any, row: any) {
+            const boton = that.botonesDatatable;
+            return boton.botonesDropdown.nativeElement.outerHTML
+              .split('$id')
+              .join(id);
+          },
+          className: 'pl-1 pr-0 text-center',
+          responsivePriority: 7,
+        },
+      ],
+      drawCallback: (settings: any) => {
+        $('.btn-dropdown-modificar')
+          .off()
+          .on('click', (event: any) => {
+            this.service
+              .getRolesFormularioById(event.target.dataset.id)
+              .subscribe(({ data }) => {
+                this.Id = data.id;
+                this.frmRolesFormulario.controls.ProductoId.setValue(
+                  data.productoId
+                );
+                this.frmRolesFormulario.controls.Estado.setValue(data.estado);
+              });
           });
-          $('.btn-dropdown-eliminar').off().on('click', (event : any) => {
+        $('.btn-dropdown-eliminar')
+          .off()
+          .on('click', (event: any) => {
             this.helperService.confirmDelete(() => {
-              this.service.delete(event.target.dataset.id).subscribe(l => {
+              this.service.delete(event.target.dataset.id).subscribe((l) => {
                 if (l.status) {
-                  this.helperService.showMessage(MessageType.SUCCESS, Messages.DELETESUCCESS);
+                  this.helperService.showMessage(
+                    MessageType.SUCCESS,
+                    Messages.DELETESUCCESS
+                  );
                   this.refrescarTabla();
                 } else {
-                  this.helperService.showMessage(MessageType.ERROR, Messages.DELETEERROR);
+                  this.helperService.showMessage(
+                    MessageType.ERROR,
+                    Messages.DELETEERROR
+                  );
                 }
-              })
+              });
             });
           });
-        }
+      },
     };
   }
-
-
 }
